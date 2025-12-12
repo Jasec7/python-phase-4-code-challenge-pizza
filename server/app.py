c90f32ba3acff6b65fa6008c8b8f4214e16e6da4
Jasec <jasec_7@hotmail.com>
2025-12-12 03:28:49 -0800

    add get method for restaurantsid, pizzas and delete method for restaurants

--- server/app.py
+++ server/app.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python3
 from models import db, Restaurant, RestaurantPizza, Pizza
 from flask_migrate import Migrate
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, jsonify
 from flask_restful import Api, Resource
 import os
 
@@ -24,6 +24,41 @@ api = Api(app)
 def index():
     return "<h1>Code challenge</h1>"
 
+class Restaurants(Resource):
+    def get(self):
+        restaurants = [restaurant.to_dict(only=('address', 'id','name')) for restaurant in Restaurant.query.all()]
+        return make_response(jsonify(restaurants), 200)
+    
+class Restaurant_id(Resource):
+    def get(self, id):
+        restaurant = Restaurant.query.filter_by(id = id).first()
+
+        if not restaurant:
+            return {'error':'Restaurant not found'}, 404
+        
+        return make_response(jsonify(restaurant.to_dict()), 200)
+
+    def delete(self, id):
+        restaurant = Restaurant.query.filter_by(id = id).first()
+
+        if not restaurant:
+            return {"error":"Restaurant not found"}, 404
+        
+        db.session.delete(restaurant)
+        db.session.commit()
+
+        return make_response("", 204)
+    
+class Pizzas(Resource):
+    def get(self):
+        pizzas = [pizza.to_dict(only=('id','ingredients','name')) for pizza in Pizza.query.all()]
+        return make_response(jsonify(pizzas), 200)
+    
+
+    
+api.add_resource(Restaurants,'/restaurants')
+api.add_resource(Restaurant_id,'/restaurants/<int:id>')
+api.add_resource(Pizzas,'/pizzas')
 
 if __name__ == "__main__":
     app.run(port=5555, debug=True)
